ce08d1fbaf770818f5bd282f756990c245e6a933
jhnmeelr <jhn.meelr@gmail.com>
2016-10-18 15:14:11 +0300

    Connecting Sockets

--- components/App.js
+++ components/App.js
@@ -1,9 +1,39 @@
 var React = require('react');
+var io = require('socket.io-client');
+
+var Header = require('./parts/Header');
 
 var App = React.createClass({
+
+    getInitialState() {
+        return {
+            status: 'disconnected'
+        }
+    },
+
+    componentWillMount() {
+        this.socket = io('http://localhost:3000');
+        this.socket.on('connect', this.connect);
+        this.socket.on('disconnect', this.disconnect);
+    },
+
+    connect() {
+        this.setState({
+            status: 'connected'
+        });
+    },
+
+    disconnect() {
+        this.setState({
+            status: 'disconnected'
+        });
+    }, 
+
     render() {
         return (
-            <h1>Hello World from React</h1>
+            <div>
+                <Header title='New Header' />
+            </div>
         );
     }
 });
